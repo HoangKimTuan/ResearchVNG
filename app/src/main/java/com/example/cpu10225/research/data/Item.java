2b4412f7e11c4f0d346bb32213591842c7dd318a
HoangkimTuan <hoangkimtuan1996@gmail.com>
2017-11-13 09:04:19 +0700

    ! fix Relations Item-Owner

--- app/src/main/java/com/example/cpu10225/research/data/Item.java
+++ app/src/main/java/com/example/cpu10225/research/data/Item.java
@@ -8,6 +8,9 @@ import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Property;
 import org.greenrobot.greendao.annotation.ToOne;
 import org.greenrobot.greendao.annotation.Unique;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.DaoException;
+import org.greenrobot.greendao.annotation.NotNull;
 
 /**
  * Created by cpu10225 on 10/11/2017.
@@ -15,11 +18,10 @@ import org.greenrobot.greendao.annotation.Unique;
 
 @Entity(nameInDb = "item")
 public class Item {
-    @Id(autoincrement = true)
+    @Id
     private Long id;
     @Property(nameInDb = "ownerId")
     private long ownerId;
-    @Property(nameInDb = "owner")
     @ToOne(joinProperty = "ownerId")
     @SerializedName("owner")
     @Expose
@@ -52,69 +54,153 @@ public class Item {
     @SerializedName("last_edit_date")
     @Expose
     private Integer lastEditDate;
-
-    public Owner getOwner() {
-        return owner;
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+    /** Used for active entity operations. */
+    @Generated(hash = 182764869)
+    private transient ItemDao myDao;
+    @Generated(hash = 1219895832)
+    public Item(Long id, long ownerId, Boolean isAccepted, Integer score,
+            Integer lastActivityDate, Integer creationDate, Integer answerId,
+            Integer questionId, Integer lastEditDate) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.isAccepted = isAccepted;
+        this.score = score;
+        this.lastActivityDate = lastActivityDate;
+        this.creationDate = creationDate;
+        this.answerId = answerId;
+        this.questionId = questionId;
+        this.lastEditDate = lastEditDate;
     }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
+    @Generated(hash = 1470900980)
+    public Item() {
     }
-
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public long getOwnerId() {
+        return this.ownerId;
+    }
+    public void setOwnerId(long ownerId) {
+        this.ownerId = ownerId;
+    }
     public Boolean getIsAccepted() {
-        return isAccepted;
+        return this.isAccepted;
     }
-
     public void setIsAccepted(Boolean isAccepted) {
         this.isAccepted = isAccepted;
     }
-
     public Integer getScore() {
-        return score;
+        return this.score;
     }
-
     public void setScore(Integer score) {
         this.score = score;
     }
-
     public Integer getLastActivityDate() {
-        return lastActivityDate;
+        return this.lastActivityDate;
     }
-
     public void setLastActivityDate(Integer lastActivityDate) {
         this.lastActivityDate = lastActivityDate;
     }
-
     public Integer getCreationDate() {
-        return creationDate;
+        return this.creationDate;
     }
-
     public void setCreationDate(Integer creationDate) {
         this.creationDate = creationDate;
     }
-
     public Integer getAnswerId() {
-        return answerId;
+        return this.answerId;
     }
-
     public void setAnswerId(Integer answerId) {
         this.answerId = answerId;
     }
-
     public Integer getQuestionId() {
-        return questionId;
+        return this.questionId;
     }
-
     public void setQuestionId(Integer questionId) {
         this.questionId = questionId;
     }
-
     public Integer getLastEditDate() {
-        return lastEditDate;
+        return this.lastEditDate;
     }
-
     public void setLastEditDate(Integer lastEditDate) {
         this.lastEditDate = lastEditDate;
     }
-
+    @Generated(hash = 1847295403)
+    private transient Long owner__resolvedKey;
+    /** To-one relationship, resolved on first access. */
+    @Generated(hash = 136625949)
+    public Owner getOwner() {
+        long __key = this.ownerId;
+        if (owner__resolvedKey == null || !owner__resolvedKey.equals(__key)) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            OwnerDao targetDao = daoSession.getOwnerDao();
+            Owner ownerNew = targetDao.load(__key);
+            synchronized (this) {
+                owner = ownerNew;
+                owner__resolvedKey = __key;
+            }
+        }
+        return owner;
+    }
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 204407784)
+    public void setOwner(@NotNull Owner owner) {
+        if (owner == null) {
+            throw new DaoException(
+                    "To-one property 'ownerId' has not-null constraint; cannot set to-one to null");
+        }
+        synchronized (this) {
+            this.owner = owner;
+            ownerId = owner.getId();
+            owner__resolvedKey = ownerId;
+        }
+    }
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 881068859)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getItemDao() : null;
+    }
 }
